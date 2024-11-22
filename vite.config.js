a85be143bdd1032bb3b661e5ca15e455ed131bd2
Rochel <liuyingfeng57@gmail.com>
2024-11-22 12:29:40 +0800

    base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/D-Perfume-react/',
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.svg'],
 })
